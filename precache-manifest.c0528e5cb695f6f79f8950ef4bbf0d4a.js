4e6d966d026833befeb8161eb833c19cd964ed02
AppVeyor <ci@appveyor.com>
2020-06-17 07:23:38 +0000

    7f5cc75

---
+++ precache-manifest.c0528e5cb695f6f79f8950ef4bbf0d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920325311b2713862fd81425b73c89ec",
+    "url": "/ra-homeworks-hooks-context/index.html"
+  },
+  {
+    "revision": "c1c8f3f3f6e20023b0a1",
+    "url": "/ra-homeworks-hooks-context/static/css/main.dc601d7a.chunk.css"
+  },
+  {
+    "revision": "bc2195aefd7dd08da447",
+    "url": "/ra-homeworks-hooks-context/static/js/2.498adaff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ra-homeworks-hooks-context/static/js/2.498adaff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c8f3f3f6e20023b0a1",
+    "url": "/ra-homeworks-hooks-context/static/js/main.98fbcdb1.chunk.js"
+  },
+  {
+    "revision": "b22765d9c958b92589f9",
+    "url": "/ra-homeworks-hooks-context/static/js/runtime-main.5d2f4a09.js"
+  }
+]);
